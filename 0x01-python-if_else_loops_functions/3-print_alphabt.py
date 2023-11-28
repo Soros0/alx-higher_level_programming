3abb45e9550820f13cd1144aed1c15e4b8741b0f
Soros0 <140617613+Soros0@users.noreply.github.com>
2023-11-28 22:43:36 +0100

    0

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-for i in range(ord('a'), ord('z') + 1):
-    if chr(i) == 'e' or chr(i) == 'q':
+for i in range(97, 123):
+    if (i == 101) or (i == 113):
         continue
-    else:
-        print("{:c}".format(i), end="")
+    print(chr(i).format(), end="")
